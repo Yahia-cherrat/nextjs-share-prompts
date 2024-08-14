f5246e9ac659150bedb8c8986e3c0ffe0fef77ad
Yahia-cherrat <78372992+Yahia-cherrat@users.noreply.github.com>
2024-08-14 17:47:55 +0100

    Remove .env file from tracking

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,9 @@
 import '@styles/globals.css'
 import Nav from '@components/Nav'
+import Provider from '@components/Provider'
 
 export const metadata = {
-    title: 'Propmtopia',
+    title: 'FeedShare',
     description: 'Discover & Share real world promots'
 }
 
@@ -10,14 +11,16 @@ const Routlayout = ({children}) => {
   return (
     <html lang="en">
         <body>
-            <div className='main' >
-                <div className='gradient' />
-            </div>
-
-            <main className="app">
-                <Nav/>
-                {children}
-            </main>
+            <Provider>
+                <div className='main' >
+                    <div className='gradient' />
+                </div>
+    
+                <main className="app">
+                    <Nav/>
+                    {children}
+                </main>
+            </Provider>
         </body>
     </html>
   )
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,3 @@
-
-
 const Home = () => {
   return (
     <section className='w-full flex-center flex-col'>
@@ -9,7 +7,7 @@ const Home = () => {
             <span className="blue_gradient text-center">AI-Powered Prompts</span>
         </h1>
         <p className="desc text-center">
-            Promptopia is an open-source platform for discovering, sharing, and creating feedback about whatever you want.
+          FeedShare is an open-source platform for discovering, sharing, and creating feedback about whatever you want.
         </p>
     </section>
   )
